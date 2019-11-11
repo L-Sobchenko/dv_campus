0b0db30a2d72d2fda2052003ea5d7997286298ac
Liudmyla Sobchenko <sobchenko_luda@ukr.net>
2019-11-12 00:12:42 +0200

    created new theme, added DB dump

---
+++ dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,32 @@
+module.exports = {
+    sobchenko_luma_en_US: {
+        area: 'frontend',
+        name: 'LSobchenko/lumasobchenko',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+    sobchenko_luma_uk_UA: {
+        area: 'frontend',
+        name: 'LSobchenko/lumasobchenko',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+    sobchenko_luma_ru_RU: {
+        area: 'frontend',
+        name: 'LSobchenko/lumasobchenko',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    }
+};
